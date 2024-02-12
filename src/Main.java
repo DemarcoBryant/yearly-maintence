8af5ad265de388524eaefdfddd09a38ec891ab5c
DemarcoBryant <158539299+DemarcoBryant@users.noreply.github.com>
2024-02-12 13:14:52 -0500

    yearly

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 4;
+        double summerCost = 8;
+        double autumnCost = 12;
+        double winterCost = 16;
+        double totalCost = springCost + summerCost + autumnCost + winterCost;
+        System.out.println("Spring maintenance cost: $" + springCost);
+        System.out.println("Summer maintenance cost: $" + summerCost);
+        System.out.println("Autumn maintenance cost: $" + autumnCost);
+        System.out.println("Winter maintenance cost: $" + winterCost);
+        System.out.println("Total yearly maintenance cost: $" + totalCost);
+    }
+}
